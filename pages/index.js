dc5b86f1b42f1779d4d445672080c7b6f6b81dba
Uditt Lamba <uditt@aetho.co>
2022-05-26 19:22:57 +0530

    started building the landing page. learning the ropes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-xl underline ">Hello world!</h1>
+    <div className="flex flex-col h-screen bg-gray-600">
+      <Head>
+        <title>admix</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <button className="mx-auto my-auto bg-green-700 p-5 drop-shadow-md rounded-2xl">
+        <h1 className="text-3xl font-bold text-xl  ">Welcome to Admix!</h1>
+      </button>
+    </div>
   );
 }
